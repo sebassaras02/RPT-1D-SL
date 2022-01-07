f180cae707a1d7c4f3d517e40238ce5f8c0aa180
root <root@dgx-node-0-0.hpc.cedia.edu.ec>
2022-01-07 08:15:57 -0500

    Incluye los resultados de 9 simulaciones

---
+++ build/CMakeFiles/naidetector.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/G4-e/pos1/action.cc" "/G4-e/pos1/build/CMakeFiles/naidetector.dir/action.cc.o"
+  "/G4-e/pos1/detectorconstruction.cc" "/G4-e/pos1/build/CMakeFiles/naidetector.dir/detectorconstruction.cc.o"
+  "/G4-e/pos1/detectormessenger.cc" "/G4-e/pos1/build/CMakeFiles/naidetector.dir/detectormessenger.cc.o"
+  "/G4-e/pos1/event.cc" "/G4-e/pos1/build/CMakeFiles/naidetector.dir/event.cc.o"
+  "/G4-e/pos1/generator.cc" "/G4-e/pos1/build/CMakeFiles/naidetector.dir/generator.cc.o"
+  "/G4-e/pos1/naidetector.cc" "/G4-e/pos1/build/CMakeFiles/naidetector.dir/naidetector.cc.o"
+  "/G4-e/pos1/physicslist.cc" "/G4-e/pos1/build/CMakeFiles/naidetector.dir/physicslist.cc.o"
+  "/G4-e/pos1/runaction.cc" "/G4-e/pos1/build/CMakeFiles/naidetector.dir/runaction.cc.o"
+  "/G4-e/pos1/stepping.cc" "/G4-e/pos1/build/CMakeFiles/naidetector.dir/stepping.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_RAYTRACERX"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/geant4/geant4-install/geant4.10.07.p02-m-install/include/Geant4"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  "/usr/include/x86_64-linux-gnu/qt5/QtPrintSupport"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- build/rpt1d_order.sh
+++ build/rpt1d_order.sh
@@ -1,95 +1,95 @@
 #!/bin/bash
 for i in {1..2036}
 do
-    ./naidetector /G4-e/rpt1d/pos1/build/macros_1/pos$i.mac
+    ./naidetector /G4-e/pos1/build/macros_1/pos$i.mac
     hadd output_f.root  *.root
     root -q hsimpleReader.C
     mv NaIDetector.dat NaIDetector_pos$i.dat
-    mv NaIDetector_pos$i.dat /G4-e/rpt1d/pos1/build/results_1
+    mv NaIDetector_pos$i.dat /G4-e/pos1/build/results_1/ 
     rm *.root
 done
 
 
 for i in {1..2036}
 do
-    ./naidetector /G4-e/rpt1d/pos1/build/macros_2/pos$i.mac
+    ./naidetector /G4-e/pos1/build/macros_2/pos$i.mac
     hadd output_f.root  *.root
     root -q hsimpleReader.C
     mv NaIDetector.dat NaIDetector_pos$i.dat
-    mv NaIDetector_pos$i.dat /G4-e/rpt1d/pos1/build/results_2
+    mv NaIDetector_pos$i.dat /G4-e/pos1/build/results_2/
     rm *.root
 done
 
 
 for i in {1..2036}
 do
-    ./naidetector /G4-e/rpt1d/pos1/build/macros_3/pos$i.mac
+    ./naidetector /G4-e/pos1/build/macros_3/pos$i.mac
     hadd output_f.root  *.root
     root -q hsimpleReader.C
     mv NaIDetector.dat NaIDetector_pos$i.dat
-    mv NaIDetector_pos$i.dat /G4-e/rpt1d/pos1/build/results_3
+    mv NaIDetector_pos$i.dat /G4-e/pos1/build/results_3/
     rm *.root
 done
 
 for i in {1..2036}
 do
-    ./naidetector /G4-e/rpt1d/pos1/build/macros_4/pos$i.mac
+    ./naidetector /G4-e/pos1/build/macros_4/pos$i.mac
     hadd output_f.root  *.root
     root -q hsimpleReader.C
     mv NaIDetector.dat NaIDetector_pos$i.dat
-    mv NaIDetector_pos$i.dat /G4-e/rpt1d/pos1/build/results_4
-    rm *.root
-done
-
-
-for i in {1..2036}
-do
-    ./naidetector /G4-e/rpt1d/pos1/build/macros_5/pos$i.mac
-    hadd output_f.root  *.root
-    root -q hsimpleReader.C
-    mv NaIDetector.dat NaIDetector_pos$i.dat
-    mv NaIDetector_pos$i.dat /G4-e/rpt1d/pos1/build/results_5
-    rm *.root
-done
-
-for i in {1..2036}
-do
-    ./naidetector /G4-e/rpt1d/pos1/build/macros_6/pos$i.mac
-    hadd output_f.root  *.root
-    root -q hsimpleReader.C
-    mv NaIDetector.dat NaIDetector_pos$i.dat
-    mv NaIDetector_pos$i.dat /G4-e/rpt1d/pos1/build/results_6
+    mv NaIDetector_pos$i.dat /G4-e/pos1/build/results_4/
     rm *.root
 done
 
 
 for i in {1..2036}
 do
-    ./naidetector /G4-e/rpt1d/pos1/build/macros_7/pos$i.mac
+    ./naidetector /G4-e/pos1/build/macros_5/pos$i.mac
     hadd output_f.root  *.root
     root -q hsimpleReader.C
     mv NaIDetector.dat NaIDetector_pos$i.dat
-    mv NaIDetector_pos$i.dat /G4-e/rpt1d/pos1/build/results_7
+    mv NaIDetector_pos$i.dat /G4-e/pos1/build/results_5/
     rm *.root
 done
 
 for i in {1..2036}
 do
-    ./naidetector /G4-e/rpt1d/pos1/build/macros_8/pos$i.mac
+    ./naidetector /G4-e/pos1/build/macros_6/pos$i.mac
     hadd output_f.root  *.root
     root -q hsimpleReader.C
     mv NaIDetector.dat NaIDetector_pos$i.dat
-    mv NaIDetector_pos$i.dat /G4-e/rpt1d/pos1/build/results_8
+    mv NaIDetector_pos$i.dat /G4-e/pos1/build/results_6/
+    rm *.root
+done
+
+
+for i in {1..2036}
+do
+    ./naidetector /G4-e/pos1/build/macros_7/pos$i.mac
+    hadd output_f.root  *.root
+    root -q hsimpleReader.C
+    mv NaIDetector.dat NaIDetector_pos$i.dat
+    mv NaIDetector_pos$i.dat /G4-e/pos1/build/results_7/
     rm *.root
 done
 
 for i in {1..2036}
 do
-    ./naidetector /G4-e/rpt1d/pos1/build/macros_9/pos$i.mac
+    ./naidetector /G4-e/pos1/build/macros_8/pos$i.mac
     hadd output_f.root  *.root
     root -q hsimpleReader.C
     mv NaIDetector.dat NaIDetector_pos$i.dat
-    mv NaIDetector_pos$i.dat /G4-e/rpt1d/pos1/build/results_9
+    mv NaIDetector_pos$i.dat /G4-e/pos1/build/results_8/
+    rm *.root
+done
+
+for i in {1..2036}
+do
+    ./naidetector /G4-e/pos1/build/macros_9/pos$i.mac
+    hadd output_f.root  *.root
+    root -q hsimpleReader.C
+    mv NaIDetector.dat NaIDetector_pos$i.dat
+    mv NaIDetector_pos$i.dat /G4-e/pos1/build/results_9/
     rm *.root
 done
 
